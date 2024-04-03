8ddde1050814536427b14dd7ce3c345edbc0bcf6
anderr <105037294+andeerrrr@users.noreply.github.com>
2024-04-04 01:02:10 +0800

    Update sign_up.php

--- sign_up.php
+++ sign_up.php
@@ -56,7 +56,7 @@ if(isset($_POST['submit'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="Assets/icon.png">
     <title>Sign Up</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="signup.css">
     <script>
         function validateForm() {
             var firstname = document.getElementById('firstname').value;
@@ -85,35 +85,57 @@ if(isset($_POST['submit'])) {
 
     <div class="container">
 
-    <form method="post" onsubmit="return validateForm()" class="square">
-        <center><h1>Create your account</h1></center>
-        <label for="firstname">First Name</label><br>
-        <center><input type="text" id="firstname" name="firstname" placeholder="Enter your name..."></center><br>
-        
-        <label for="lastname">Last Name</label><br>
-        <center><input type="text" id="lastname" name="lastname" placeholder="Enter your lastname..."></center><br>
-        
-        <label for="username">Username</label><br>
-        <center><input type="text" id="username" name="username" placeholder="Username"></center><br>
-        
-        <label for="password">Password</label><br>
-        <center><input type="password" id="password" name="password" placeholder="Password"></center><br>
-        
-        <label for="confirm_password">Confirm Password</label><br>
-        <center><input type="password" id="confirm_password" name="confirm_password" placeholder="Confirm Password"></center><br>
-        
-        <label for="sex">Sex:</label>
-        <select id="sex" name="sex" required>
-            <option value="">Select</option>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-        </select>
-        
-        <label for="dob">Date of Birth:</label>
-        <input type="date" id="dob" name="dob" autocomplete="off" required><br>
-        <center><input id="spbt" type="submit" value="Create account" name="submit"></center><br>
-        <a href="login.php" >Already have an account?</a>
-    </form>
+        <form method="post" onsubmit="return validateForm()" class="square">
+            <center><h1>Create your account</h1></center>
+            <div id="firstrow">
+                <div class="fn">
+                    <label for="firstname">First Name</label><br>
+                    <input type="text" id="firstname" name="firstname" placeholder="Enter your name..."><br>
+                </div>
+                <div class="ln">
+                    <label for="lastname">Last Name</label><br>
+                    <input type="text" id="lastname" name="lastname" placeholder="Enter your lastname..."><br>
+                </div>
+            </div>
+            <div id="firstrow" style="margin: -5% 0 0 0;">
+                <div class="fn">
+                    <label for="username">Username</label><br>
+                    <center><input type="text" id="username" name="username" placeholder="Username"></center><br>
+                </div>
+                <div class="ln">
+                    <label for="password">Password</label><br>
+                    <center><input type="password" id="password" name="password" placeholder="Password"></center><br>
+                </div>
+            </div>
+            <div>
+            
+                <div id="secrow">
+                    <div id="options">
+                         <div class="adjust">
+                             <label for="sex">Sex:</label><br>
+                             <select id="sex" name="sex" required>
+                                <option value="">Select</option>
+                                <option value="male">Male</option>
+                                <option value="female">Female</option>
+                             </select>
+                         </div>
+                         <div class="adjust">
+                             <label for="dob" id="doblabel">Date of Birth:</label><br>
+                             <input type="date" id="dob" name="dob" autocomplete="off" required><br>
+                         </div>
+                    </div>
+
+                    <div id="conpass">
+                        <label for="confirm_password">Confirm Password</label><br>
+                        <input type="password" id="confirm_password" name="confirm_password" placeholder="Confirm Password"><br>
+                    </div>
+                </div>
+
+            
+            </div>
+            <center><input id="spbt" type="submit" value="Create account" name="submit"></center><br>
+            <a href="login.php" >Already have an account?</a>
+        </form>
     </div>
 </body>
 </html>
