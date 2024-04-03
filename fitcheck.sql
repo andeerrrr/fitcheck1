6498e4a341b728a92ff5c10fff83c09a00ab8266
anderr <105037294+andeerrrr@users.noreply.github.com>
2024-04-04 02:53:05 +0800

    Update fitcheck.sql

--- fitcheck.sql
+++ fitcheck.sql
@@ -1,34 +1,108 @@
--- Create Users table
-CREATE TABLE users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    firstname VARCHAR(255) NOT NULL,
-    lastname VARCHAR(255) NOT NULL,
-    username VARCHAR(255) NOT NULL,
-    pword VARCHAR(255) NOT NULL,
-    sex VARCHAR(6) NOT NULL,
-    dob DATE,
-    profile_picture VARCHAR(255) -- Add column for profile picture
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 03, 2024 at 08:50 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
--- Insert sample values into Users table
-INSERT INTO users (firstname, lastname, username, pword, sex, dob) VALUES
-('andre', 'tanalgo', 'admin','123','male','1990-01-01'),
-('Alquinn John', 'Undar','Plmhjuein','timetrialist1170','male,','2003,02,26');
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Create Workouts table with an additional column for image URLs
-    CREATE TABLE workouts (
-        workout_id INT PRIMARY KEY,
-        workout_name VARCHAR(255) NOT NULL,
-        workout_muscle_group VARCHAR(255) NOT NULL,
-        workout_category VARCHAR(255) NOT NULL,
-        workout_description TEXT,
-        image_url VARCHAR(255)
-    );
+--
+-- Database: `fitcheck`
+--
 
--- Insert sample values into Workouts table with image URLs
-INSERT INTO workouts (workout_id, workout_name, workout_muscle_group, workout_category, workout_description, image_url)
-VALUES 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `routines`
+--
+
+CREATE TABLE `routines` (
+  `routine_id` int(11) NOT NULL,
+  `routine_name` varchar(255) NOT NULL,
+  `routine_description` text DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `routines`
+--
+
+INSERT INTO `routines` (`routine_id`, `routine_name`, `routine_description`, `user_id`) VALUES
+(1, 'WEIGHTLOSS PROGRAM', 'Revolutionize your weight loss journey with our personalized program. Say goodbye to fad diets and hello to sustainable results. Lets transform together!', 1),
+(2, 'MOBILITY PROGRAM', 'Unlock freedom with our mobility program. Tailored exercises and expert guidance help you move better and live stronger. Say hello to a more agile you!', 1),
+(3, 'BODYBUILDING PROGRAM', 'Transform your physique with our bodybuilding program. Customized workouts and expert guidance help you pack on muscle and reach your goals. Lets build together!', 1),
+(4, 'CARDIOVASCULAR ENDURANCE', 'Boost your endurance with our cardio program. Tailored workouts and expert guidance enhance your stamina and vitality. Ready to conquer any challenge!', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `routine_workouts`
+--
+
+CREATE TABLE `routine_workouts` (
+  `id` int(11) NOT NULL,
+  `routine_id` int(11) NOT NULL,
+  `workout_id` int(11) NOT NULL,
+  `rows` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `pword` varchar(255) NOT NULL,
+  `sex` varchar(6) NOT NULL,
+  `dob` date DEFAULT NULL,
+  `profile_picture` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `firstname`, `lastname`, `username`, `pword`, `sex`, `dob`, `profile_picture`) VALUES
+(1, 'Andre', 'Tanalgo', 'anderrr', '123', 'male', '2003-09-19', NULL),
+(2, 'Alquinn John', 'Undar', 'Plmhjuein', 'timetrialist1170', 'male,', '2003-02-26', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `workouts`
+--
+
+CREATE TABLE `workouts` (
+  `workout_id` int(11) NOT NULL,
+  `workout_name` varchar(255) NOT NULL,
+  `workout_muscle_group` varchar(255) NOT NULL,
+  `workout_category` varchar(255) NOT NULL,
+  `workout_description` text DEFAULT NULL,
+  `image_url` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `workouts`
+--
+
+INSERT INTO `workouts` (`workout_id`, `workout_name`, `workout_muscle_group`, `workout_category`, `workout_description`, `image_url`) VALUES
 (1, 'Bench Press', 'Chest', 'Upper Body', 'Compound exercise targeting chest, triceps, and shoulders. Lie on back, unrack bar, lower to chest, and push back up.', 'images/workout_images/00251201-Barbell-Bench-Press_Chest.gif'),
 (2, 'Deadlifts', 'Full Body', 'Lower Body', 'Full-body exercise working posterior chain, including lower back, glutes, and hamstrings. Stand with barbell, bend at hips and knees, grasp bar, and lift by extending hips and knees.', 'images/workout_images/00321201-Barbell-Deadlift_Hips-FIX.gif'),
 (3, 'Squats', 'Legs', 'Lower Body', 'Fundamental lower-body exercise targeting quads, hamstrings, and glutes. Stand with feet shoulder-width apart, lower body by bending knees, and push back up.', 'images/workout_images/11971201-Squat-m_Thighs.gif'),
@@ -54,7 +128,6 @@ VALUES
 (23, 'Dumbbell Lunges', 'Legs', 'Lower Body', 'Work legs. Step forward with one foot, lower body until both knees are bent, and return to starting position.', 'images/workout_images/03361201-Dumbbell-Lunge_Hips.gif'),
 (24, 'Barbell Squats', 'Legs', 'Lower Body', 'Compound lower body exercise targeting quads, hamstrings, and glutes. Stand with barbell on upper back, lower body into a squat position, and return to standing.', 'images/workout_images/00431201-Barbell-Full-Squat_Thighs.gif'),
 (25, 'Bent Over Rows', 'Back', 'Upper Body', 'Target mid-back. Bend at hips, hold dumbbells, and pull elbows back, squeezing shoulder blades together.', 'images/workout_images/02931201-Dumbbell-Bent-Over-Row_Back.gif'),
-
 (27, 'Calf Raises', 'Calves', 'Lower Body', 'Isolate calf muscles. Stand on raised surface, lift heels by pushing through toes, and lower them back down.', 'images/workout_images/13731101-Bodyweight-Standing-Calf-Raise_Calves_small.jpg'),
 (28, 'Cable Crunches', 'Abdominals', 'Upper Body', 'Target abdominal muscles. Sit at cable machine, grasp rope attachment, and crunch forward, squeezing abs.', 'images/workout_images/01751201-Cable-Kneeling-Crunch_Waist.gif'),
 (29, 'Russian Twists', 'Obliques', 'Upper Body', 'Target oblique muscles. Sit on floor, lean back slightly, and rotate torso to touch ground on each side.', 'images/workout_images/06871201-Russian-Twist_waist.gif'),
@@ -76,39 +149,108 @@ VALUES
 (45, 'Preacher Curls', 'Biceps', 'Upper Body', 'Isolate biceps. Sit at preacher curl bench, curl barbell or dumbbells toward shoulders, and lower them back down.', 'images/workout_images/1421577_preacher_curls.gif'),
 (46, 'Tricep Rope Pushdowns', 'Triceps', 'Upper Body', 'Isolate triceps. Attach rope to cable machine, push it down until arms are fully extended, and return to starting position.', 'images/workout_images/tricep_rope_pushdowns.jpg');
 
--- Create UserRoutines table for the many-to-many relationship
-CREATE TABLE routines (
-    routine_id INT AUTO_INCREMENT PRIMARY KEY,
-    routine_name VARCHAR(255) NOT NULL,
-    routine_description TEXT,
-    user_id INT,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
+-- --------------------------------------------------------
 
--- Insert sample values into UserRoutines table
-INSERT INTO routines (routine_id, routine_name,routine_description,user_id) VALUES
-(1,'WEIGHTLOSS PROGRAM','Revolutionize your weight loss journey with our personalized program. Say goodbye to fad diets and hello to sustainable results. Lets transform together!',1),
-(2,'MOBILITY PROGRAM', 'Unlock freedom with our mobility program. Tailored exercises and expert guidance help you move better and live stronger. Say hello to a more agile you!',1),
-(3,'BODYBUILDING PROGRAM', 'Transform your physique with our bodybuilding program. Customized workouts and expert guidance help you pack on muscle and reach your goals. Lets build together!',1),
-(4,'CARDIOVASCULAR ENDURANCE', 'Boost your endurance with our cardio program. Tailored workouts and expert guidance enhance your stamina and vitality. Ready to conquer any challenge!',1);
+--
+-- Table structure for table `workout_data`
+--
 
-CREATE TABLE routine_workouts(
-    routine_id INT,
-    user_id INT,
-    workout_id INT,
-    reps INT,
-    volume INT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (routine_id) REFERENCES routines(routine_id),
-    FOREIGN KEY (workout_id) REFERENCES workouts(workout_id)
-);
+CREATE TABLE `workout_data` (
+  `id` int(11) NOT NULL,
+  `num` int(11) NOT NULL,
+  `routine_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `workout_id` int(11) NOT NULL,
+  `reps` int(11) NOT NULL,
+  `volume` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Insert sample data into routine_workouts
-INSERT INTO routine_workouts (routine_id, user_id, workout_id, reps, volume, created_at) 
-VALUES (1, 1, 1, 10, 20, DEFAULT);
+--
+-- Dumping data for table `workout_data`
+--
 
--- Select data from routine_workouts with corresponding workout names
-SELECT routine_workouts.*, workouts.workout_name 
-FROM routine_workouts 
-JOIN workouts ON routine_workouts.workout_id = workouts.workout_id;
+INSERT INTO `workout_data` (`id`, `num`, `routine_id`, `user_id`, `workout_id`, `reps`, `volume`, `created_at`) VALUES
+(1, 2, 1, 1, 4, 8, 30, '2024-04-03 18:16:38'),
+(2, 3, 1, 1, 1, 8, 45, '2024-04-03 18:41:29'),
+(3, 3, 1, 1, 2, 8, 70, '2024-04-03 18:41:29'),
+(4, 1, 1, 1, 4, 5, 10, '2024-04-03 18:41:29'),
+(5, 1, 1, 1, 15, 10, 70, '2024-04-03 18:41:29');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `routines`
+--
+ALTER TABLE `routines`
+  ADD PRIMARY KEY (`routine_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `routine_workouts`
+--
+ALTER TABLE `routine_workouts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `workouts`
+--
+ALTER TABLE `workouts`
+  ADD PRIMARY KEY (`workout_id`);
+
+--
+-- Indexes for table `workout_data`
+--
+ALTER TABLE `workout_data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `routines`
+--
+ALTER TABLE `routines`
+  MODIFY `routine_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `routine_workouts`
+--
+ALTER TABLE `routine_workouts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `workout_data`
+--
+ALTER TABLE `workout_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `routines`
+--
+ALTER TABLE `routines`
+  ADD CONSTRAINT `routines_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
